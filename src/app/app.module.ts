987e002fe77a630f8e35f0bfe9cc9f4ec3d619ce
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-01-24 18:10:14 -0300

    Implementando funcionalidade de criação e autentificação de usuário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { ToastModule } from 'primeng/toast';
 import { HomeComponent } from './modules/home/home.component';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,7 @@ import { HomeComponent } from './modules/home/home.component';
     ButtonModule,
     ToastModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,10 @@
+import { CookieService } from 'ngx-cookie-service';
+import { UserService } from './../../services/user/user.service';
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
+import { SignupUserResponse } from 'src/app/models/interfaces/user/SignupUserResponse';
+import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
 
 @Component({
   selector: 'app-home',
@@ -20,13 +25,42 @@ export class HomeComponent {
     password: ['', Validators.required],
   });
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private UserService: UserService,
+    private CookieService: CookieService,
+    ) {}
 
   OnSubmitLoginForm(): void {
-    console.log('DADOS DO FORMULARIO DE LOGIN', this.loginForm.value);
+    if (this.loginForm.value && this.loginForm.valid){
+      this.UserService.authUser(this.loginForm.value as AuthRequest)
+      .subscribe({
+        next: (response) => {
+          if (response){
+            this.CookieService.set('USER_INFO', response?.token);
+
+            this.loginForm.reset();
+          }
+        },
+        error: (err) => console.log(err),
+      });
+    }
   }
 
   OnSubmitSignupForm(): void {
-    console.log('DADOS DO FORMULARIO DE CRIAÇÃO DE CONTA', this.signupForm.value);
+    if (this.signupForm.value && this.signupForm.valid){
+      this.UserService
+      .signupUser(this.signupForm.value as SignupUserRequest)
+      .subscribe({
+        next: (response) => {
+          if(response) {
+            alert('Usuário teste criado com sucesso!');
+            this,this.signupForm.reset();
+            this.loginCard = true;
+          }
+        },
+        error: (err) => console.log(err),
+      });
+    }
   }
 }
