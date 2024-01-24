fa153e56a48aaa78983383e7bc88518928de16e2
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-01-24 17:01:37 -0300

    Implementando formulário reativo na tela de login

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  OnSubmitLoginForm(): void {
+    console.log('DADOS DO FORMULARIO DE LOGIN', this.loginForm.value);
+  }
+
+  OnSubmitSignupForm(): void {
+    console.log('DADOS DO FORMULARIO DE CRIAÇÃO DE CONTA', this.signupForm.value);
+  }
 }
