bdb0c18648fd56cd52097a59cb3ec386128ded53
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-01-25 16:35:25 -0300

    Criando página de dashboard, componente e rotas da respectiva

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { DashboardHomeComponent } from './modules/dashboard/page/dashboard-home/dashboard-home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
-  }
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+ {
+  path: 'home',
+  component: HomeComponent
+ },
+ {
+  path: 'dashboard',
+  component: DashboardHomeComponent
+ }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
